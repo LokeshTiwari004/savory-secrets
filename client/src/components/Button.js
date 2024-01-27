11930dd7f0b2d4f08692e60d4f33fca63e45ae12
LokeshTiwari004 <lokeshtiwari001vns@gmail.com>
2024-01-28 00:20:34 +0530

    initial work on Discover Page

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function Button() {
+function Button({ value }) {
   return (
-    <button>Log In</button>
+    <button>{value}</button>
   )
 }
 
--- client/src/components/Discover.js
+++ client/src/components/Discover.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Search from './Search'
+import ToggleButton from './ToggleButton'
+import Filter from './Filter'
 
 function Discover() {
+  // const [foodList, setFoodList] =useState([])
+  // useEffect()
   return (
-    <div>Discover</div>
+    <div>
+      <Search />
+      <ToggleButton toggleValue={["Veg", "Non-Veg"]} />
+      <Filter filterBy="country" />
+    </div>
   )
 }
 
