a1792bbb8d6c8e18432262c94e8a8cbbdac8a724
LokeshTiwari004 <lokeshtiwari001vns@gmail.com>
2024-01-29 17:15:31 +0530

    ---

--- app/app.dev.py
+++ app/app.dev.py
@@ -2,7 +2,6 @@ import base64
 import os
 from flask import Flask, make_response, jsonify, send_file, send_from_directory
 from flask_cors import CORS  #, cross_origin
-# from load_json import load_json
 
 app = Flask(__name__)
 cors = CORS(app, resources={
@@ -14,17 +13,9 @@ cors = CORS(app, resources={
 _home_page = "./static/home.json"
 _about_page = "./static/about.json"
 _food_image = "./assets/food.png"
-if os.path.isFile(_food_image):
-  with open(_food_image, 'rb') as img_file:
-    _food_image = jsonify({
-      "success": 1,
-      "image": base64.b64encode(img_file).decode('utf-8')})
-else:
-  _food_image = jsonify({"success": 0})
 
 @app.route("/api/home-page")
 def home_page():
-  # return _home_page, 200
   return send_file(_home_page), 200
 
 @app.route("/api/about-page")
@@ -33,7 +24,7 @@ def about_page():
 
 @app.route("/api/food-image")
 def food_image():
-  return _food_image, 200
+  return send_file(_food_image), 200
 
 
 if __name__ == "__main__":
